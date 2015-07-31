2c882b9a1c7c11e2d9e4bd542a904d7cad9d3ba3
Ramon Portela <portela.ramon96@gmail.com>
2015-07-31 20:36:17 -0300

    V0.7

--- src/com/montadora/Veiculo.java
+++ src/com/montadora/Veiculo.java
@@ -1,11 +1,52 @@
 package com.montadora;
 
 public abstract class Veiculo {
-	
-	String chassi;
-	String montadora;
-	String modelo;
-	String tipo;
-	String cor;
 
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	protected String chassi;
+	protected String montadora;
+	protected String modelo;
+	protected String tipo;
+	protected String cor;	
+	
+	public String getChassi() {
+		return chassi;
+	}
+
+	public void setChassi(String chassi) {
+		this.chassi = chassi;
+	}
+
+	public String getMontadora() {
+		return montadora;
+	}
+
+	public void setMontadora(String montadora) {
+		this.montadora = montadora;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	protected abstract void criarVeiculo();
 }
