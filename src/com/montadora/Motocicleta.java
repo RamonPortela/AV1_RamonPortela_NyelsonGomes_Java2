c11cdca5d1c2087b1ecc4d2d8f84efab0566277c
Nyelson Gomes Barbosa <nyelsonbarbosa@hotmail.com>
2015-08-12 16:17:28 -0300

    v1.3.0.1

--- src/com/montadora/Motocicleta.java
+++ src/com/montadora/Motocicleta.java
@@ -1,11 +1,27 @@
 package com.montadora;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import com.utilitarios.Excecoes;
+import com.utilitarios.Impressora;
+import com.utilitarios.MetodosAuxiliares;
 
 public class Motocicleta extends Veiculo {
 	
 	private int cilindrada;
 	private int capacidadeDoTanque;
+	
+	private int validarCilindrada(int cilindrada){
+		if(cilindrada <= 0 || cilindrada > 999){
+			this.cilindrada = cilindrada;
+		}
+		else{
+			System.out.println("Cilindrada invalida.");
+			return 0;
+		}
+		return cilindrada;
+	}
 
 	public enum Tipos{//enum de tipos de motos
 		SCOOTER(1, "Scooter"), CUSTOM(2, "Custom"), CHOPPER(3, "Chopper"), STREET(4, "Street"), ESPORTIVA(5, "Esportiva");
@@ -105,7 +121,7 @@ public class Motocicleta extends Veiculo {
 	}
 	
 	public void setCilindrada(int cilindrada) {
-		this.cilindrada = cilindrada;
+		validarCilindrada(cilindrada);
 	}
 	
 	public int getCapacidadeDoTanque() {
@@ -118,28 +134,133 @@ public class Motocicleta extends Veiculo {
 	
 	@Override
 	public Motocicleta criarVeiculo(Scanner input) {// método que cria moto a partir do que foi informado pelo usuário
+		
 		Motocicleta moto = new Motocicleta();
+		boolean deuExcecao = false;
+		int opcaoMontadora;
+		int opcaoTipo;
+		int opcaoCor;
+		int opcaoCilindrada = 0;
+		int opcaoCambio;
 		
 		System.out.println("Entre com o chassi do veículo:");
 		moto.setChassi(input.next());
-		System.out.println("Escolha uma montadora: ");
-		System.out.println("1 - HARLEY-DAVIDSON   \t 2 - HONDA \t 3 - SUZUKI \t 4 - KAWASAKI \t 5 - SHINERAY");
-		moto.setMontadora(input.nextInt());
-		System.out.println("Entre com o tipo do veículo: ");
-		System.out.println("1 - Scooter   \t 2 - Custom \t 3 - Roadster \t 4 - Street \t 5 - Esportiva");
-		moto.setTipo(input.nextInt());
-		input.nextLine();
+		
+		String texto = "Escolha uma montadora: ";
+		String opcoes = "1 - HARLEY-DAVIDSON   \t 2 - HONDA \t 3 - SUZUKI \t 4 - KAWASAKI \t 5 - SHINERAY";
+		
+		do{
+			System.out.println(texto);
+			System.out.println(opcoes);
+			opcaoMontadora = Excecoes.lancaExcecaoOpcoesVeiculo(input, texto, opcoes);
+			if((opcaoMontadora < 1) || (opcaoMontadora > 5)){
+				Impressora.imprimeOpcaoInvalida();
+				MetodosAuxiliares.pressionarEnterErro();
+			}else{
+				moto.setMontadora(opcaoMontadora);
+			}
+			
+		}while((opcaoMontadora < 1) || (opcaoMontadora > 5));
+		
+		texto = "Entre com o tipo do veículo: ";
+		opcoes = "1 - Scooter   \t 2 - Custom \t 3 - Roadster \t 4 - Street \t 5 - Esportiva";
+		do{
+			System.out.println(texto);
+			System.out.println(opcoes);
+			opcaoTipo = Excecoes.lancaExcecaoOpcoesVeiculo(input, texto, opcoes);
+			if((opcaoTipo < 1) || (opcaoTipo > 5)){
+				Impressora.imprimeOpcaoInvalida();
+				MetodosAuxiliares.pressionarEnterErro();					
+			}else{
+				moto.setTipo(opcaoTipo);	
+			}			
+			
+		}while((opcaoTipo < 1) || (opcaoTipo > 5));
+		
+		input.nextLine(); // Não sei pra que que serve.
+		
 		System.out.println("Entre com o modelo do veículo:");
 		moto.setModelo(input.nextLine());
-		System.out.println("Selecione a cor do veículo: ");
-		System.out.println("1 - azul   \t 2 - vermelho \t 3 - branco \t 4 - preto \t 5 - verde");
-		moto.setCor(input.nextInt());
-		System.out.println("Entre com as cilindradas da moto: ");
-		moto.setCilindrada(input.nextInt());
+		
+		texto = "Selecione a cor do veículo: ";
+		opcoes = "1 - azul   \t 2 - vermelho \t 3 - branco \t 4 - preto \t 5 - verde";
+		do{
+			System.out.println(texto);
+			System.out.println(opcoes);
+			opcaoCor = Excecoes.lancaExcecaoOpcoesVeiculo(input, texto, opcoes);
+			if((opcaoCor < 1) || (opcaoCor > 5)){
+				Impressora.imprimeOpcaoInvalida();
+				MetodosAuxiliares.pressionarEnterErro();
+			}else{
+				moto.setCor(opcaoCor);	
+			}				
+			
+		}while((opcaoCor < 1) || (opcaoCor > 5));
+		
+		texto = "Entre com as cilindradas da moto: ";
+		opcoes = "Limite de 3 numeros de cilindradas.";
+		
+		do{
+			deuExcecao = false;
+			System.out.println(texto);
+			System.out.println(opcoes);
+
+			try{
+				moto.setCilindrada(input.nextInt());
+			}catch(InputMismatchException e){
+				System.out.println("O preço deve ser um número.");
+				MetodosAuxiliares.pressionarEnterErro();
+				deuExcecao = true;
+				input.nextLine();
+			}catch(Exception e){
+				Impressora.imprimeErroInesperado();
+				MetodosAuxiliares.pressionarEnterErro();
+				deuExcecao = true;
+				input.nextLine();
+			}
+		}
+		while(deuExcecao);
+		
+		/*do{
+			deuExcecao = false;
+			System.out.println("Entre com as cilindradas da moto: ");
+			try{
+				moto.setCilindrada(input.nextInt());	
+			}catch(InputMismatchException e){
+				System.out.println("O preço deve ser um número.");
+				MetodosAuxiliares.pressionarEnterErro();
+				deuExcecao = true;
+				input.nextLine();
+			}catch(Exception e){
+				Impressora.imprimeErroInesperado();
+				MetodosAuxiliares.pressionarEnterErro();
+				deuExcecao = true;
+				input.nextLine();
+			}
+					
+		}while(deuExcecao);*/
+
 		System.out.println("Entre com a capacidade do taque de combustível: ");
 		moto.setCapacidadeDoTanque(input.nextInt());
-		System.out.println("Entre com o preço do veículo: ");
-		moto.setPreco(input.nextFloat());
+		
+		do{
+			deuExcecao = false;
+			System.out.println("Entre com o preço do veículo: ");
+			try{
+				moto.setPreco(input.nextFloat());	
+			}catch(InputMismatchException e){
+				System.out.println("O preço deve ser um número.");
+				MetodosAuxiliares.pressionarEnterErro();
+				deuExcecao = true;
+				input.nextLine();
+			}catch(Exception e){
+				Impressora.imprimeErroInesperado();
+				MetodosAuxiliares.pressionarEnterErro();
+				deuExcecao = true;
+				input.nextLine();
+			}
+					
+		}while(deuExcecao);
 		
 		return moto;
 	}
